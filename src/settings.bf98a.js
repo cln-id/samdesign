9019cc7302391e6d416ed9006d298b18a52747c7
cln-id <32337736+cln-id@users.noreply.github.com>
2022-07-14 11:18:46 +0800

    Add files via upload

---
+++ src/settings.bf98a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/01_shouYe/01_shouYe.fire",orientation:"",jsList:["assets/01_shouYe/jiaoBen/jumpScene.c8b90.js"],bundleVers:{internal:"1b082",main:"eb2ca"}};
